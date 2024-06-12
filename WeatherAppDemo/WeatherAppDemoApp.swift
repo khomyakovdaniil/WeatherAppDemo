4c8ab8ec8882d6e1061008b216a3a4035be9b1f9
KhomyakovDaniil <daniil.h@appomart.com>
2024-06-12 12:35:09 +0300

    Initial Commit

---
+++ WeatherAppDemo/WeatherAppDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppDemoApp.swift
+//  WeatherAppDemo
+//
+//  Created by  Даниил Хомяков on 12.06.2024.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
